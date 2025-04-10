62aab887f2d497b4f0de06ba3f942d4430ab1b27
Mariane Anjos <mariane.ferreiraanjos@outlook.com>
2025-04-10 17:12:03 -0300

    Exercicios de estrutura FOR

--- logica_de_programacao/Estrutura_for_laboratorio.java
+++ logica_de_programacao/Estrutura_for_laboratorio.java
@@ -1,5 +1,4 @@
 package logica_de_programacao;
-
 import java.util.Scanner;
 
 /*Maria acabou de iniciar seu curso de graduação na faculdade de medicina e precisa de sua ajuda para organizar os experimentos de um laboratório o qual ela é responsável. Ela quer saber no final do ano, quantas cobaias foram utilizadas no laboratório e o percentual de cada tipo de cobaia utilizada.
